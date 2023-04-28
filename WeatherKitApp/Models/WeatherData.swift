6ea328ebfcf01e04af4ff2ae3bdcc25c2371dd6e
tokomac <tokomatokomac@gmail.com>
2023-04-28 21:05:12 +0900

    Second Commit : Using Combine

--- WeatherKitApp/Models/WeatherData.swift
+++ WeatherKitApp/Models/WeatherData.swift
@@ -8,21 +8,41 @@
 import SwiftUI
 import WeatherKit
 import CoreLocation
+import Combine
 
-struct WeatherData {
+class WeatherData: ObservableObject {
     
     static let shared = WeatherData()
     private let service = WeatherService.shared
+    var cityObserver: AnyCancellable?
+    @Published var currentWeather: CurrentWeather?
+    @Published var dailyForecast: Forecast<DayWeather>?
+    @Published var hourlyForecast: Forecast<HourWeather>?
+    @ObservedObject var locationManager: LocationManager = LocationManager()
     
-    func attributionInfoData() async -> WeatherAttribution?  {
-        let _attributionInfo = await Task.detached(priority: .userInitiated) {
-            let _attribution = try? await self.service.attribution
-            return _attribution
-        }.value
-        return _attributionInfo
+    deinit {
+        cityObserver?.cancel()
     }
     
-    func weatherForecast(userLocation: CLLocation) async -> (CurrentWeather?, Forecast<HourWeather>?, Forecast<DayWeather>?) {
+    init() {
+        getForcast()
+    }
+    
+    func getForcast() {
+        cityObserver = locationManager.$location.receive(on: DispatchQueue.main)
+            .sink { _loc in
+                Task.detached { @MainActor in
+                    if let _location = _loc {
+                        let forcast = await WeatherData.shared.weatherForecast(userLocation: _location)
+                        self.currentWeather = forcast.0
+                        self.hourlyForecast = forcast.1
+                        self.dailyForecast = forcast.2
+                    }
+                }
+            }
+    }
+ 
+    private func weatherForecast(userLocation: CLLocation) async -> (CurrentWeather?, Forecast<HourWeather>?, Forecast<DayWeather>?) {
         let _forecast = await Task.detached(priority: .userInitiated) {
             let forcast = try? await self.service.weather(
                 for: userLocation,
